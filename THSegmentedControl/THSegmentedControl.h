99e2356179d2c347b9e4a6cadb0af53eebd8de41
Rogerio de Paula Assis <rogerio@freshmob.com.au>
2016-09-09 15:25:05 +1000

    THSegmentedControl framework

---
+++ THSegmentedControl/THSegmentedControl.h
@@ -0,0 +1,19 @@
+//
+//  THSegmentedControl.h
+//  THSegmentedControl
+//
+//  Created by Rogerio de Paula Assis on 9/09/2016.
+//  Copyright © 2016 Freshmob Pty Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for THSegmentedControl.
+FOUNDATION_EXPORT double THSegmentedControlVersionNumber;
+
+//! Project version string for THSegmentedControl.
+FOUNDATION_EXPORT const unsigned char THSegmentedControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <THSegmentedControl/PublicHeader.h>
+
+#import <THSegmentedControl/THSegmentedControl.h>
