6638140e61e4f8848c075df2481e803917d7a99e
Rogerio de Paula Assis <rogerio@freshmob.com.au>
2016-09-09 14:58:49 +1000

    Segmented Control framework

---
+++ SegmentedControlFramework/SegmentedControlFramework.h
@@ -0,0 +1,22 @@
+//
+//  SegmentedControlFramework.h
+//  SegmentedControlFramework
+//
+//  Created by Rogerio de Paula Assis on 9/09/2016.
+//  Copyright © 2016 Freshmob Pty Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SegmentedControlFramework.
+FOUNDATION_EXPORT double SegmentedControlFrameworkVersionNumber;
+
+//! Project version string for SegmentedControlFramework.
+FOUNDATION_EXPORT const unsigned char SegmentedControlFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SegmentedControlFramework/PublicHeader.h>
+
+#import <SegmentedControlFramework/THSegmentedControl.h>
+
+
+
